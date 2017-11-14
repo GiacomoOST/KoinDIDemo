d40e5453be0629267b8d3b839aad256eff862184
GiacomoOST <giacomo.palopoli@ostmodern.co.uk>
2017-11-14 14:26:19 +0000

    Added property injection

--- app/src/main/java/com/ostmodern/koininjectiondemo/MainActivity.kt
+++ app/src/main/java/com/ostmodern/koininjectiondemo/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.ostmodern.koininjectiondemo
 
+import android.app.Application
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.ostmodern.koininjectiondemo.viewmodels.MainViewModel
 import org.koin.android.ext.android.inject
+import org.koin.android.ext.android.property
 
 class MainActivity : AppCompatActivity() {
 
     // Inject my ViewModel
     val mainViewModel by inject<MainViewModel>("Temporary")
+
+    val applicationContext by property<Application>("ApplicationContext")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/ostmodern/koininjectiondemo/MainApplication.kt
+++ app/src/main/java/com/ostmodern/koininjectiondemo/MainApplication.kt
@@ -2,6 +2,7 @@ package com.ostmodern.koininjectiondemo
 
 import android.app.Application
 import com.ostmodern.koininjectiondemo.modules.MainModule
+import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.android.startAndroidContext
 
 class MainApplication : Application() {
@@ -9,6 +10,8 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startAndroidContext(this, appModules())
+
+        getKoin().setProperty("ApplicationContext", this)
     }
 
     fun appModules() = listOf( MainModule() )
